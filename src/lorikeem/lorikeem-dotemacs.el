f07b1dbf5df7231f5bf423e6cda1dc62cf7dfaf2
John Willis <jwillis@coherent-logic.com>
2013-05-13 08:08:42 -0600

    commit

--- src/lorikeem/lorikeem-dotemacs.el
+++ src/lorikeem/lorikeem-dotemacs.el
@@ -1,7 +1,10 @@
 
-;//LORIKEEM
-(add-to-list 'load-path "~/.emacs.d")
+;;//LORIKEEM START
+(add-to-list 'load-path "~/.emacs.d/plugins/lorikeem")
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas/global-mode 1)
 (autoload 'mumps-mode "lorikeem" "MUMPS mode" t)
 (setq auto-mode-alist (append '(("\\.m$" . mumps-mode)) auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.ewd$" . xml-mode)) auto-mode-alist))
-;LORIKEEM//
+;;LORIKEEM END//
